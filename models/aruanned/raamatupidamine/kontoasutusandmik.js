49df4b0437c9147d8ca78d0b2149c78a12f94bcc
Vladislav Gordin <vladislav.gordin@gmail.com>
2018-08-29 17:30:23 +0300

    Ведомость по суб.счетам, отчет

---
+++ models/aruanned/raamatupidamine/kontoasutusandmik.js
@@ -0,0 +1,50 @@
+module.exports = {
+    grid: {
+        gridConfiguration: [
+            {id: "konto", name: "konto", width: "25px", show: false},
+            {id: "nimetus", name: "Nimetus", width: "100px"},
+            {id: "alg_db", name: "Alg.Deebet", width: "100px"},
+            {id: "alg_kr", name: "Alg.Kreedit", width: "75px"},
+            {id: "deebet", name: "Deebet", width: "100px"},
+            {id: "kreedit", name: "Kreedit", width: "100px"},
+            {id: "lopp_db", name: "Lõpp deebet", width: "100px"},
+            {id: "lopp_kr", name: "Lõpp kreedit", width: "200px"}
+        ],
+        sqlString: `  SELECT
+                        sum(deebet)
+                          FILTER (WHERE kpv is null) OVER (
+                          PARTITION BY rekv_id, asutus_id, konto ) AS alg_saldo,
+                        CASE WHEN kpv is not null
+                          THEN deebet
+                        ELSE 0 END                                 AS deebet,
+                        kreedit,
+                        sum(deebet - kreedit)
+                        OVER (
+                          PARTITION BY rekv_id, asutus_id, konto ) AS lopp_saldo,
+                        qryReport.kpv,
+                        qryReport.rekv_id,
+                        qryReport.asutus_id,
+                        qryReport.konto,
+                        qryReport.korr_konto,
+                        qryReport.dok,
+                        qryReport.number,
+                        qryReport.kood1,
+                        qryReport.kood2,
+                        qryReport.kood3,
+                        qryReport.kood4,
+                        qryReport.kood5,
+                        qryReport.proj,
+                        qryReport.tunnus,
+                        a.regkood, a.nimetus as asutus, a.tp,
+                        l.nimetus,
+                        r.nimetus as rekv_nimetus,
+                        r.parentid 
+                      FROM docs.kontoasutusandmik($1::text, $2 :: INTEGER, $3::date, $4 :: DATE, $5::integer) qryReport
+                      inner join com_asutused a on a.id = qryReport.asutus_id
+                      inner join com_kontoplaan l on l.kood = qryReport.konto
+                      inner join com_rekv r on r.id = qryReport.rekv_id
+                      where qryReport.konto is not null`,     // $1- konto, $2 - asutus_id, $3 - kpv1, $4 - kpv2, $5 - rekvid (svod)
+        params: '',
+        alias: 'kontoasutusandmik_report'
+    }
+};
---
+++ sql/aruanned/raamatupidamine/kontoasutusandmik.sql
@@ -0,0 +1,183 @@
+DROP FUNCTION IF EXISTS docs.kontoasutusandmik( TEXT, INTEGER, DATE, DATE, INTEGER );
+
+CREATE OR REPLACE FUNCTION docs.kontoasutusandmik(l_konto  TEXT, l_asutus INTEGER, l_kpv1 DATE, l_kpv2 DATE,
+                                                  l_rekvid INTEGER)
+  RETURNS TABLE(
+    rekv_id    INTEGER,
+    asutus_id  INTEGER,
+    kpv        DATE,
+    deebet     NUMERIC(14, 2),
+    kreedit    NUMERIC(14, 2),
+    konto      VARCHAR(20),
+    korr_konto VARCHAR(20),
+    dok        VARCHAR(120),
+    number     INTEGER,
+    kood1      VARCHAR(20),
+    kood2      VARCHAR(20),
+    kood3      VARCHAR(20),
+    kood4      VARCHAR(20),
+    kood5      VARCHAR(20),
+    proj       VARCHAR(20),
+    tunnus     VARCHAR(20)
+  ) AS
+$BODY$
+WITH algsaldo AS (
+    SELECT
+      rekv_id,
+      sum(deebet) - sum(kreedit) AS alg_saldo,
+      asutus_id,
+      konto
+    FROM (
+           SELECT
+             d.rekvid            AS rekv_id,
+             asutusid            AS asutus_id,
+             deebet              AS konto,
+             summa               AS deebet,
+             0 :: NUMERIC(14, 2) AS kreedit
+           FROM docs.doc d
+             INNER JOIN docs.journal j ON j.parentid = d.id
+             INNER JOIN docs.journal1 j1 ON j1.parentid = j.id
+           WHERE j.asutusid IS NOT NULL
+                 AND (empty(l_asutus)  OR j.asutusid = l_asutus)
+                 AND j.kpv < l_kpv1
+                 AND j.rekvid IN (SELECT rekv_id
+                                  FROM get_asutuse_struktuur(l_rekvid))
+           UNION ALL
+           SELECT
+             d.rekvid            AS rekv_id,
+             asutusid            AS asutus_id,
+             kreedit             AS konto,
+             0 :: NUMERIC(14, 2) AS deebet,
+             summa               AS kreedit
+           FROM docs.doc d
+             INNER JOIN docs.journal j ON j.parentid = d.id
+             INNER JOIN docs.journal1 j1 ON j1.parentid = j.id
+           WHERE j.asutusid IS NOT NULL
+                 AND (empty(l_asutus) OR j.asutusid = l_asutus)
+                 AND j.kpv < l_kpv1
+                 AND j.rekvid IN (SELECT rekv_id
+                                  FROM get_asutuse_struktuur(l_rekvid))
+         ) qry
+    WHERE (empty(l_konto) OR qry.konto LIKE ltrim(rtrim(l_konto)) || '%')
+    GROUP BY rekv_id, asutus_id, konto)
+SELECT
+  a.rekv_id,
+  a.asutus_id,
+  null::date           AS kpv,
+  a.alg_saldo          AS deebet,
+  0 :: NUMERIC(14, 2)  AS kreedit,
+  a.konto,
+  NULL :: VARCHAR(20)  AS korr_konto,
+  NULL :: VARCHAR(120) AS dok,
+  NULL :: INTEGER      AS number,
+  NULL :: VARCHAR(20)  AS kood1,
+  NULL :: VARCHAR(20)  AS kood2,
+  NULL :: VARCHAR(20)  AS kood3,
+  NULL :: VARCHAR(20)  AS kood4,
+  NULL :: VARCHAR(20)  AS kood5,
+  NULL :: VARCHAR(20)  AS proj,
+  NULL :: VARCHAR(20)  AS tunnus
+FROM algsaldo a
+UNION ALL
+SELECT
+  qry.rekv_id,
+  qry.asutus_id,
+  qry.kpv,
+  qry.deebet  AS deebet,
+  qry.kreedit AS kreedit,
+  qry.konto,
+  qry.korr_konto,
+  qry.dok,
+  qry.number,
+  qry.kood1,
+  qry.kood2,
+  qry.kood3,
+  qry.kood4,
+  qry.kood5,
+  qry.proj,
+  qry.tunnus
+FROM (
+       SELECT
+         j.rekvid     AS rekv_id,
+         j.kpv,
+         j.summa      AS deebet,
+         0 :: NUMERIC AS kreedit,
+         j.deebet     AS konto,
+         j.kreedit    AS korr_konto,
+         j.dok,
+         j.number,
+         j.asutusid   AS asutus_id,
+         j.kood1,
+         j.kood2,
+         j.kood3,
+         j.kood4,
+         j.kood5,
+         j.proj,
+         j.tunnus
+       FROM cur_journal j
+       WHERE j.asutusid IS NOT NULL
+             AND (empty(l_asutus) OR j.asutusid = l_asutus)
+             AND j.kpv >= l_kpv1
+             AND j.kpv <= l_kpv2
+             AND j.rekvid IN (SELECT rekv_id
+                              FROM get_asutuse_struktuur(l_rekvid))
+       UNION ALL
+       SELECT
+         j.rekvid     AS rekv_id,
+         j.kpv,
+         0 :: NUMERIC AS deebet,
+         j.summa      AS kreedit,
+         j.kreedit    AS konto,
+         j.deebet     AS korr_konto,
+         j.dok,
+         j.number,
+         j.asutusid   AS asutus_id,
+         j.kood1,
+         j.kood2,
+         j.kood3,
+         j.kood4,
+         j.kood5,
+         j.proj,
+         j.tunnus
+       FROM cur_journal j
+       WHERE j.asutusid IS NOT NULL
+             AND (empty(l_asutus) OR j.asutusid = l_asutus)
+             AND j.kpv >= l_kpv1
+             AND j.kpv <= l_kpv2
+             AND j.rekvid IN (SELECT rekv_id
+                              FROM get_asutuse_struktuur(l_rekvid))
+     ) qry
+WHERE (empty(l_konto) OR qry.konto LIKE ltrim(rtrim(l_konto)) || '%');
+
+$BODY$
+LANGUAGE SQL VOLATILE
+COST 100;
+
+/*
+  SELECT
+    sum(deebet)
+      FILTER (WHERE kpv is null) OVER (
+      PARTITION BY rekv_id, asutus_id, konto ) AS alg_saldo,
+    CASE WHEN kpv is not null
+      THEN deebet
+    ELSE 0 END                                 AS deebet,
+    kreedit,
+    sum(deebet - kreedit)
+    OVER (
+      PARTITION BY rekv_id, asutus_id, konto ) AS lopp_saldo,
+    kpv,
+    rekv_id,
+    asutus_id,
+    konto,
+    korr_konto,
+    dok,
+    number,
+    kood1,
+    kood2,
+    kood3,
+    kood4,
+    kood5,
+    proj,
+    tunnus
+  FROM docs.kontoasutusandmik(null::text, NULL :: INTEGER, '2018-06-10', current_date :: DATE, 1)
+*/
