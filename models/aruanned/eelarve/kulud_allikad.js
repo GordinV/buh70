8bba13628c3299ccf71abb3001f62f90c370bdc4
Vladislav Gordin <vladislav.gordin@gmail.com>
2018-09-05 19:08:06 +0300

    Бюджетные расходы, отчеты

---
+++ models/aruanned/eelarve/kulud_allikad.js
@@ -0,0 +1,29 @@
+module.exports = {
+    grid: {
+        gridConfiguration: [
+            {id: "konto", name: "konto", width: "25px", show: false},
+            {id: "nimetus", name: "Nimetus", width: "100px"},
+            {id: "alg_db", name: "Alg.Deebet", width: "100px"},
+            {id: "alg_kr", name: "Alg.Kreedit", width: "75px"},
+            {id: "deebet", name: "Deebet", width: "100px"},
+            {id: "kreedit", name: "Kreedit", width: "100px"},
+            {id: "lopp_db", name: "Lõpp deebet", width: "100px"},
+            {id: "lopp_kr", name: "Lõpp kreedit", width: "200px"}
+        ],
+        sqlString: `SELECT
+                        qryReport.*,
+                        a.nimetus,
+                        t.nimetus as tegev_nimetus,
+                        r.regkood,
+                        r.nimetus AS asutus,
+                        p.regkood AS parregkood,
+                        p.nimetus AS parasutus
+                    FROM eelarve.eelarve_kulud($1::integer, $2::date, $3::boolean, $4::integer) qryReport
+                    LEFT OUTER JOIN com_artikkel a ON ltrim(rtrim(a.kood)) = ltrim(rtrim(qryReport.artikkel))
+                    LEFT OUTER JOIN com_tegev t ON ltrim(rtrim(t.kood)) = ltrim(rtrim(qryReport.tegev))
+                    INNER JOIN ou.rekv r ON r.id = qryReport.rekv_id
+                    LEFT OUTER JOIN ou.rekv p ON r.parentid = p.id                    `,     // $1 - aasta $2 - kpv, $3 - parandus, $4 - rekvid (svod)
+        params: '',
+        alias: 'kulud_report'
+    }
+};
---
+++ sql/aruanned/eelarve/eelarve.eelarve_kulud.sql
@@ -0,0 +1,77 @@
+DROP FUNCTION IF EXISTS eelarve.eelarve_kulud( INTEGER, DATE, BOOLEAN, INTEGER );
+
+CREATE OR REPLACE FUNCTION eelarve.eelarve_kulud(l_aasta INTEGER, l_kpv DATE, is_parandus BOOLEAN, l_rekvid INTEGER)
+  RETURNS TABLE(
+    rekv_id  INTEGER,
+    eelarve  NUMERIC(14, 2),
+    kassa    NUMERIC(14, 2),
+    tegelik  NUMERIC(14, 2),
+    artikkel VARCHAR(20),
+    allikas  VARCHAR(20),
+    tegev    VARCHAR(20),
+    tunnus   VARCHAR(20)
+  ) AS
+$BODY$
+SELECT
+  rekvid,
+  sum(eelarve) AS eelarve,
+  sum(tegelik) AS tegelik,
+  sum(kassa)   AS kassa,
+  tegev,
+  allikas,
+  artikkel,
+  tunnus
+FROM (
+       SELECT
+         rekvid,
+         summa        AS eelarve,
+         0 :: NUMERIC AS tegelik,
+         0 :: NUMERIC AS kassa,
+         kood1        AS tegev,
+         kood2        AS allikas,
+         kood5        AS artikkel,
+         tunnus
+       FROM eelarve.kulud e
+       WHERE e.rekvid IN (SELECT rekv_id
+                          FROM get_asutuse_struktuur(l_rekvid))
+             AND aasta = l_aasta
+             AND (empty(is_parandus) OR (e.kpv IS NULL OR e.kpv <= l_kpv))
+       UNION ALL
+       SELECT
+         rekvid,
+         0 :: NUMERIC AS eelarve,
+         summa        AS tegelik,
+         0 :: NUMERIC AS kassa ,
+         tegev,
+         allikas,
+         artikkel,
+         tunnus
+       FROM cur_kulude_taitmine ft
+       WHERE ft.rekvid IN (SELECT rekv_id
+                           FROM get_asutuse_struktuur(l_rekvid))
+             AND ft.aasta = l_aasta
+             AND ft.kuu <= month(l_kpv)
+       UNION ALL
+       SELECT
+         rekvid,
+         0 :: NUMERIC AS eelarve,
+         0 :: NUMERIC AS tegelik,
+         summa        AS kassa,
+         tegev,
+         allikas,
+         artikkel,
+         tunnus
+       FROM cur_kulude_kassa_taitmine kt
+       WHERE kt.rekvid IN (SELECT rekv_id
+                           FROM get_asutuse_struktuur(l_rekvid))
+             AND kt.aasta = l_aasta
+             AND kt.kuu <= month(l_kpv)) qry
+GROUP BY rekvid, tegev, allikas, artikkel, tunnus;
+
+$BODY$
+LANGUAGE SQL VOLATILE
+COST 100;
+
+SELECT *
+FROM eelarve.eelarve_kulud(2018, '2018-01-01', TRUE, 1)
+
