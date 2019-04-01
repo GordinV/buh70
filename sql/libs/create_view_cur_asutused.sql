be2f05e7631a278ff4813be986078f9b0214c11f
Vladislav Gordin <vladislav.gordin@gmail.com>
2019-04-01 22:59:04 +0300

    Справочник контр.агентов

--- sql/libs/create_view_cur_asutused.sql
+++ sql/libs/create_view_cur_asutused.sql
@@ -1,16 +1,21 @@
 drop view if exists cur_asutused;
 
 CREATE VIEW cur_asutused AS
-  SELECT
-    a.id,
-    a.regkood,
-    a.nimetus,
-    a.omvorm,
-    a.aadress,
-    a.tp,
-    a.email,
-    a.mark,
-    ((a.properties ->> 'kehtivus' :: TEXT)) :: DATE AS kehtivus,
-    a.staatus
-  FROM libs.asutus a
-  WHERE (a.staatus <> 3);
+SELECT
+  a.id,
+  a.regkood,
+  a.nimetus,
+  a.omvorm,
+  a.aadress,
+  a.tp,
+  a.email,
+  a.mark,
+  coalesce((a.properties ->> 'kehtivus' :: TEXT)::date,current_date + INTERVAL '10 years') :: DATE AS kehtivus,
+  a.staatus
+FROM libs.asutus a
+WHERE (a.staatus <> 3);
+
+
+GRANT SELECT on cur_asutused to dbpeakasutaja;
+GRANT SELECT on cur_asutused to dbkasutaja;
+GRANT SELECT on cur_asutused to dbvaatleja;
