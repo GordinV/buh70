9e48b939e4d4ecc9e2a4cee36cce7f226b606aaf
Vladislav Gordin <vladislav.gordin@gmail.com>
2019-10-25 18:41:34 +0300

    task 4480 ettemaksu arve tasumine

--- sql/raama/docs.sp_salvesta_arvtasu.sql
+++ sql/raama/docs.sp_salvesta_arvtasu.sql
@@ -3,132 +3,137 @@ DROP FUNCTION IF EXISTS docs.sp_salvesta_arvtasu(JSON, INTEGER, INTEGER);
 CREATE OR REPLACE FUNCTION docs.sp_salvesta_arvtasu(data JSON,
                                                     userid INTEGER,
                                                     user_rekvid INTEGER)
-  RETURNS INTEGER AS
+    RETURNS INTEGER AS
 $BODY$
 
 DECLARE
-  lib_id          INTEGER;
-  userName        TEXT;
-  doc_id          INTEGER        = data ->> 'id';
-  doc_data        JSON           = data ->> 'data';
-  doc_rekvid      INTEGER        = doc_data ->> 'rekvid';
-  doc_doc_arv_id  INTEGER        = doc_data ->> 'doc_arv_id';
-  doc_doc_tasu_id INTEGER        = doc_data ->> 'doc_tasu_id';
-  doc_kpv         DATE           = doc_data ->> 'kpv';
-  doc_summa       NUMERIC(14, 2) = doc_data ->> 'summa';
-  doc_dok         TEXT           = doc_data ->> 'dok';
-  doc_pankkassa   INTEGER        = doc_data ->> 'pankkassa';
-  doc_muud        TEXT           = doc_data ->> 'muud';
-  v_tasu_dok      RECORD;
-  is_import       BOOLEAN        = data ->> 'import';
+    lib_id          INTEGER;
+    userName        TEXT;
+    doc_id          INTEGER        = data ->> 'id';
+    doc_data        JSON           = data ->> 'data';
+    doc_rekvid      INTEGER        = doc_data ->> 'rekvid';
+    doc_doc_arv_id  INTEGER        = doc_data ->> 'doc_arv_id';
+    doc_doc_tasu_id INTEGER        = doc_data ->> 'doc_tasu_id';
+
+    doc_kpv         DATE           = doc_data ->> 'kpv';
+    doc_summa       NUMERIC(14, 2) = doc_data ->> 'summa';
+    doc_pankkassa   INTEGER        = doc_data ->> 'pankkassa';
+    doc_muud        TEXT           = doc_data ->> 'muud';
+    v_tasu_dok      RECORD;
+    is_import       BOOLEAN        = data ->> 'import';
 
 BEGIN
 
-  IF (doc_id IS NULL)
-  THEN
-    doc_id = doc_data ->> 'id';
-  END IF;
-
-  SELECT kasutaja
-         INTO userName
-  FROM ou.userid u
-  WHERE u.rekvid = user_rekvid
-    AND u.id = userId;
-
-  IF is_import IS NULL AND userName IS NULL
-  THEN
-    RAISE NOTICE 'User not found %, user_rekvid %, userId %', userName, user_rekvid, userId;
-    RETURN 0;
-  END IF;
-
-  -- вставка или апдейт docs.doc
-  IF doc_id IS NULL OR doc_id = 0
-  THEN
-
-    RAISE NOTICE 'doc_doc_arv_id %', doc_doc_arv_id;
-    -- check if tasu from journal
-    IF doc_doc_arv_id IS NULL OR empty(doc_doc_arv_id) AND NOT empty(doc_doc_tasu_id) AND doc_pankkassa = 3
+    IF (doc_id IS NULL)
     THEN
+        doc_id = doc_data ->> 'id';
+    END IF;
 
-      --1 open jounal doc
-      SELECT *
-             INTO v_tasu_dok
-      FROM docs.journal
-      WHERE parentid = doc_doc_tasu_id;
+    SELECT kasutaja INTO userName
+    FROM ou.userid u
+    WHERE u.rekvid = user_rekvid
+      AND u.id = userId;
 
-      -- 2 find arv doc
-      doc_doc_arv_id = (SELECT parentid
-                        FROM docs.arv
-                        WHERE rekvid = v_tasu_dok.rekvid
-                          AND ltrim(rtrim(number)) = ltrim(rtrim(v_tasu_dok.dok))
-                          AND arv.asutusid = v_tasu_dok.asutusid
-                        ORDER BY jaak DESC
-                        LIMIT 1
-      );
-
-      IF doc_doc_arv_id IS NULL
-      THEN
-        RAISE NOTICE 'tasu dok ei leidnud';
+    IF is_import IS NULL AND userName IS NULL
+    THEN
+        RAISE NOTICE 'User not found %, user_rekvid %, userId %', userName, user_rekvid, userId;
         RETURN 0;
-      END IF;
-
-
     END IF;
 
     -- 3 delete old payment
 
-    DELETE
-    FROM docs.arvtasu
-    WHERE doc_tasu_id = doc_doc_tasu_id
-      AND pankkassa = doc_pankkassa;
-
-    INSERT INTO docs.arvtasu (rekvid, doc_arv_id, doc_tasu_id, kpv, summa, muud, status, pankkassa)
-    VALUES (doc_rekvid, doc_doc_arv_id, doc_doc_tasu_id, doc_kpv, doc_summa, doc_muud, 1, doc_pankkassa)
-           RETURNING id
-             INTO lib_id;
-  ELSE
-
-    UPDATE docs.arvtasu
-    SET
-      doc_arv_id  = doc_doc_arv_id,
-      doc_tasu_id = doc_doc_tasu_id,
-      kpv         = doc_kpv,
-      summa       = doc_summa,
-      muud        = doc_muud,
-      status      = CASE
-                      WHEN status = 3
-                        THEN 1
-                      ELSE status END
-    WHERE id = doc_id
-      RETURNING id
-        INTO lib_id;
-  END IF;
-
-  -- установить связи
-  -- добавим сязь счета и оплаты
-  UPDATE docs.doc SET docs_ids = array_append(docs_ids, doc_doc_tasu_id) WHERE id = doc_doc_arv_id;
-
-  -- добавим связь оплаты со счетом
-  UPDATE docs.doc SET docs_ids = array_append(docs_ids, doc_doc_arv_id) WHERE id = doc_doc_tasu_id;
-
-
-  -- update arv jaak
-  PERFORM docs.sp_update_arv_jaak(doc_doc_arv_id);
-
-  RETURN lib_id;
-
-  EXCEPTION
-  WHEN OTHERS
+    IF doc_doc_arv_id IS NOT NULL
     THEN
-      RAISE NOTICE 'error % %', SQLERRM, SQLSTATE;
-      RETURN 0;
+        DELETE
+        FROM docs.arvtasu
+        WHERE doc_tasu_id = doc_doc_tasu_id
+          AND doc_doc_arv_id NOT IN (SELECT doc_doc_arv_id -- не удаляем оплату текущего счета
+                                     UNION
+                                     SELECT unnest(d.docs_ids)
+                                     FROM docs.doc d
+                                     WHERE d.id = doc_doc_arv_id
+        ) -- не удаляем связанные счета (предоплата)
+          AND pankkassa = doc_pankkassa;
+    END IF;
+    -- вставка или апдейт docs.doc
+    IF doc_id IS NULL OR doc_id = 0
+    THEN
+
+        -- check if tasu from journal
+        IF doc_doc_arv_id IS NULL OR empty(doc_doc_arv_id) AND NOT empty(doc_doc_tasu_id) AND doc_pankkassa = 3
+        THEN
+
+            --1 open jounal doc
+            SELECT * INTO v_tasu_dok
+            FROM docs.journal
+            WHERE parentid = doc_doc_tasu_id;
+
+            -- 2 find arv doc
+            SELECT d.id
+                   INTO doc_doc_arv_id
+            FROM docs.doc d
+                     INNER JOIN docs.arv a ON a.parentid = d.id
+            WHERE d.rekvid = v_tasu_dok.rekvid
+              AND ltrim(rtrim(number)) = ltrim(rtrim(v_tasu_dok.dok))
+              AND a.asutusid = v_tasu_dok.asutusid
+            ORDER BY jaak DESC
+            LIMIT 1;
+
+            IF doc_doc_arv_id IS NULL
+            THEN
+                RAISE NOTICE 'tasu dok ei leidnud';
+                RETURN 0;
+            END IF;
+
+
+        END IF;
+
+
+        INSERT INTO docs.arvtasu (rekvid, doc_arv_id, doc_tasu_id, kpv, summa, muud, status, pankkassa)
+        VALUES (doc_rekvid, doc_doc_arv_id, doc_doc_tasu_id, doc_kpv, doc_summa, doc_muud, 1,
+                doc_pankkassa) RETURNING id
+                   INTO lib_id;
+    ELSE
+
+        UPDATE docs.arvtasu
+        SET doc_arv_id  = doc_doc_arv_id,
+            doc_tasu_id = doc_doc_tasu_id,
+            kpv         = doc_kpv,
+            summa       = doc_summa,
+            muud        = doc_muud,
+            status      = CASE
+                              WHEN status = 3
+                                  THEN 1
+                              ELSE status END
+        WHERE id = doc_id RETURNING id
+            INTO lib_id;
+    END IF;
+
+    -- установить связи
+    -- добавим сязь счета и оплаты
+    UPDATE docs.doc SET docs_ids = array_append(docs_ids, doc_doc_tasu_id) WHERE id = doc_doc_arv_id;
+
+    -- добавим связь оплаты со счетом
+    UPDATE docs.doc SET docs_ids = array_append(docs_ids, doc_doc_arv_id) WHERE id = doc_doc_tasu_id;
+
+
+    -- update arv jaak
+    PERFORM docs.sp_update_arv_jaak(doc_doc_arv_id);
+
+    RETURN lib_id;
+
+EXCEPTION
+    WHEN OTHERS
+        THEN
+            RAISE NOTICE 'error % %', SQLERRM, SQLSTATE;
+            RETURN 0;
 
 
 END;
 $BODY$
-  LANGUAGE plpgsql
-  VOLATILE
-  COST 100;
+    LANGUAGE plpgsql
+    VOLATILE
+    COST 100;
 
 GRANT EXECUTE ON FUNCTION docs.sp_salvesta_arvtasu(JSON, INTEGER, INTEGER) TO dbkasutaja;
 GRANT EXECUTE ON FUNCTION docs.sp_salvesta_arvtasu(JSON, INTEGER, INTEGER) TO dbpeakasutaja;
--- sql/raama/docs.sp_tasu_arv.sql
+++ sql/raama/docs.sp_tasu_arv.sql
@@ -1,96 +1,158 @@
 -- Function: docs.sp_salvesta_mk(json, integer, integer)
 
-DROP FUNCTION IF EXISTS docs.sp_tasu_arv( INTEGER );
-DROP FUNCTION IF EXISTS docs.sp_tasu_arv( INTEGER, INTEGER, INTEGER );
+DROP FUNCTION IF EXISTS docs.sp_tasu_arv(INTEGER);
+DROP FUNCTION IF EXISTS docs.sp_tasu_arv(INTEGER, INTEGER, INTEGER);
 
-CREATE OR REPLACE FUNCTION docs.sp_tasu_arv(
-  l_tasu_id INTEGER, l_arv_id INTEGER, l_user_id INTEGER)
-  RETURNS INTEGER AS
+CREATE OR REPLACE FUNCTION docs.sp_tasu_arv(l_tasu_id INTEGER, l_arv_id INTEGER, l_user_id INTEGER)
+    RETURNS INTEGER AS
 $BODY$
 
 DECLARE
-  l_doc_id         INTEGER;
-  v_tasu           RECORD;
-  v_params         RECORD;
-  json_object      JSONB;
-  l_tasu_type      INTEGER = 3; -- muud (lausend)
-  l_summa          NUMERIC = 0;
-  l_doc_tasu_id    INTEGER;
-  TYPE_DOK_MK      INTEGER = 1;
-  TYPE_DOK_KORDER  INTEGER = 2;
-  TYPE_DOK_JOURNAL INTEGER = 3;
+    l_doc_id         INTEGER;
+    v_tasu           RECORD;
+    v_params         RECORD;
+    json_object      JSONB;
+    l_tasu_type      INTEGER = 3; -- muud (lausend)
+    l_summa          NUMERIC = 0;
+    l_doc_tasu_id    INTEGER;
+    TYPE_DOK_MK      INTEGER = 1;
+    TYPE_DOK_KORDER  INTEGER = 2;
+    TYPE_DOK_JOURNAL INTEGER = 3;
+    v_arv            RECORD;
+    v_tulu_arved     RECORD;
+    l_tasu_summa     NUMERIC = 0;
 BEGIN
-  SELECT
-    d.*,
-    l.kood AS doc_type
-  INTO v_tasu
-  FROM docs.doc d
-    INNER JOIN libs.library l ON l.id = d.doc_type_id
-  WHERE d.id = l_tasu_id;
+    SELECT d.id, d.docs_ids, a.properties ->> 'tyyp' AS tyyp
+    into v_arv
+    FROM docs.doc d
+             INNER JOIN docs.arv a ON a.parentid = d.id
+    WHERE d.id = l_arv_id;
 
-  IF l_tasu_id IS NULL
-  THEN
-    -- Документ не найден
-    RETURN 0;
-  END IF;
+    SELECT d.*,
+           l.kood AS doc_type
+           INTO v_tasu
+    FROM docs.doc d
+             INNER JOIN libs.library l ON l.id = d.doc_type_id
+    WHERE d.id = l_tasu_id;
 
-  l_tasu_type = (CASE
-                 WHEN v_tasu.doc_type ILIKE '%MK%'
-                   THEN TYPE_DOK_MK
-                 WHEN v_tasu.doc_type ILIKE '%ORDER%'
-                   THEN TYPE_DOK_KORDER
-                 ELSE TYPE_DOK_JOURNAL END);
+    IF l_tasu_id IS NULL
+    THEN
+        -- Документ не найден
+        RETURN 0;
+    END IF;
 
-  l_summa = (
-    SELECT sum(summa) AS summa
-    FROM (
-           SELECT summa
-           FROM docs.mk m
-             INNER JOIN docs.mk1 m1 ON m.id = m1.parentid
-           WHERE m.parentid = l_tasu_id
-           UNION ALL
-           SELECT summa
-           FROM docs.korder1 k
-           WHERE k.parentid = l_tasu_id
-           UNION ALL
-           SELECT summa
-           FROM docs.journal j
-             INNER JOIN docs.journal1 j1 ON j.id = j1.parentid
-           WHERE j.parentid = l_tasu_id
-         ) tasud
-  );
+    l_tasu_type = (CASE
+                       WHEN v_tasu.doc_type ILIKE '%MK%'
+                           THEN TYPE_DOK_MK
+                       WHEN v_tasu.doc_type ILIKE '%ORDER%'
+                           THEN TYPE_DOK_KORDER
+                       ELSE TYPE_DOK_JOURNAL END);
 
-  l_doc_tasu_id = (
-    SELECT id
-    FROM docs.arvtasu
-    WHERE rekvid = v_tasu.rekvid AND doc_arv_id = l_arv_id AND doc_tasu_id = l_tasu_id
-  );
+    l_summa = (
+        SELECT sum(summa) AS summa
+        FROM (
+                 SELECT summa
+                 FROM docs.mk m
+                          INNER JOIN docs.mk1 m1 ON m.id = m1.parentid
+                 WHERE m.parentid = l_tasu_id
+                 UNION ALL
+                 SELECT summa
+                 FROM docs.korder1 k
+                 WHERE k.parentid = l_tasu_id
+                 UNION ALL
+                 SELECT summa
+                 FROM docs.journal j
+                          INNER JOIN docs.journal1 j1 ON j.id = j1.parentid
+                 WHERE j.parentid = l_tasu_id
+             ) tasud
+    );
 
-  SELECT
-    coalesce(l_doc_tasu_id, 0) AS id,
-    v_tasu.rekvid              AS rekvid,
-    l_arv_id                   AS doc_arv_id,
-    v_tasu.created :: DATE     AS kpv,
-    l_tasu_type                AS pankkassa,
-    -- 1 - mk, 2- kassa, 3 - lausend
-    l_tasu_id                  AS doc_tasu_id,
-    l_summa                    AS summa
-  INTO v_params;
+    l_doc_tasu_id = (
+        SELECT id
+        FROM docs.arvtasu
+        WHERE rekvid = v_tasu.rekvid
+          AND doc_arv_id = l_arv_id
+          AND doc_tasu_id = l_tasu_id
+    );
 
-  SELECT row_to_json(row)
-  INTO json_object
-  FROM (SELECT
-          0        AS id,
-          v_params AS data) row;
+    SELECT coalesce(l_doc_tasu_id, 0) AS id,
+           v_tasu.rekvid              AS rekvid,
+           l_arv_id                   AS doc_arv_id,
+           v_tasu.created :: DATE     AS kpv,
+           l_tasu_type                AS pankkassa,
+           -- 1 - mk, 2- kassa, 3 - lausend
+           l_tasu_id                  AS doc_tasu_id,
+           l_summa                    AS summa
+           INTO v_params;
 
-  SELECT docs.sp_salvesta_arvtasu(json_object :: JSON, l_user_id, v_tasu.rekvid)
-  INTO l_doc_id;
+    SELECT row_to_json(row) INTO json_object
+    FROM (SELECT 0        AS id,
+                 v_params AS data) row;
 
-  RETURN l_doc_id;
+    SELECT docs.sp_salvesta_arvtasu(json_object :: JSON, l_user_id, v_tasu.rekvid) INTO l_doc_id;
 
-END;$BODY$
-LANGUAGE plpgsql VOLATILE
-COST 100;
+    -- если счет имеет тип - предоплата
+
+    IF v_arv.tyyp = 'ETTEMAKS'
+    THEN
+        -- выбираем связанные счета
+        FOR v_tulu_arved IN
+            SELECT d.id, a.jaak
+            FROM docs.doc d
+                     INNER JOIN docs.arv a ON d.id = a.parentid
+            WHERE d.id IN (
+                SELECT unnest(v_arv.docs_ids)
+            )
+              AND a.jaak > 0
+            ORDER BY kpv
+            LOOP
+                -- делаем пропорциональную оплату
+                -- изем уже имеющуюся оплату
+                l_doc_tasu_id = (
+                    SELECT id
+                    FROM docs.arvtasu
+                    WHERE rekvid = v_tasu.rekvid
+                      AND doc_arv_id = v_tulu_arved.id
+                      AND doc_tasu_id = l_tasu_id
+                );
+
+                -- готовим параметры
+                l_tasu_summa = CASE WHEN v_tulu_arved.jaak > l_summa THEN l_summa ELSE v_tulu_arved.jaak END;
+                SELECT coalesce(l_doc_tasu_id, 0) AS id,
+                       v_tasu.rekvid              AS rekvid,
+                       v_tulu_arved.id            AS doc_arv_id,
+                       v_tasu.created :: DATE     AS kpv,
+                       l_tasu_type                AS pankkassa,
+                       -- 1 - mk, 2- kassa, 3 - lausend
+                       l_tasu_id                  AS doc_tasu_id,
+                       l_tasu_summa               AS summa
+                       INTO v_params;
+
+                SELECT row_to_json(row) INTO json_object
+                FROM (SELECT 0        AS id,
+                             v_params AS data) row;
+                -- созранение
+                l_doc_id = docs.sp_salvesta_arvtasu(json_object :: JSON, l_user_id, v_tasu.rekvid);
+
+                l_summa = l_summa - l_tasu_summa;
+                IF l_summa <= 0
+                THEN
+                    -- вся оплата списана, вызодим их оплат
+                    EXIT;
+                END IF;
+
+            END LOOP;
+
+    END IF;
+
+    RETURN l_doc_id;
+
+END;
+$BODY$
+    LANGUAGE plpgsql
+    VOLATILE
+    COST 100;
 
 GRANT EXECUTE ON FUNCTION docs.sp_tasu_arv(INTEGER, INTEGER, INTEGER) TO dbkasutaja;
 GRANT EXECUTE ON FUNCTION docs.sp_tasu_arv(INTEGER, INTEGER, INTEGER) TO dbpeakasutaja;
+GRANT EXECUTE ON FUNCTION docs.sp_tasu_arv(INTEGER, INTEGER, INTEGER) TO arvestaja;
