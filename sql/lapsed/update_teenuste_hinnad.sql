f30ffbb7c72f9dd2250a0febc4968bfdfe9bd788
Vladislav Gordin <vladislav.gordin@gmail.com>
2019-11-15 00:00:45 +0200

    Task 4624 update hinnad in libs

--- sql/lapsed/update_teenuste_hinnad.sql
+++ sql/lapsed/update_teenuste_hinnad.sql
@@ -19,13 +19,62 @@ DECLARE
                            LIMIT 1);
 
     v_kaart     RECORD;
+    v_groups    RECORD;
     json_object JSONB;
 
     DOC_STATUS  INTEGER = 1; -- только активные услуги
     l_count     INTEGER = 0;
     l_kaart_id  INTEGER;
+    l_grupp_id  INTEGER;
 BEGIN
     doc_type_id = 'LAPSE_KAART';
+
+    -- делаем выборку из груп
+
+    FOR v_groups IN
+        SELECT id,
+               kood,
+               nimetus,
+               muud,
+               (properties::JSONB -> 'all_yksused' ->> 1)::TEXT AS all_yksus_1,
+               (properties::JSONB -> 'all_yksused' ->> 2)::TEXT AS all_yksus_2,
+               (properties::JSONB -> 'all_yksused' ->> 3)::TEXT AS all_yksus_3,
+               (properties::JSONB -> 'all_yksused' ->> 4)::TEXT AS all_yksus_4,
+               (properties::JSONB -> 'all_yksused' ->> 5)::TEXT AS all_yksus_5,
+               (SELECT json_agg(to_jsonb(qry.*)) AS gridData
+                FROM (
+                         SELECT nomid AS id, x.nomid, x.kogus, n.hind, n.kood::text, n.nimetus::text
+                         FROM jsonb_to_recordset(
+                                      (properties::JSONB -> 'teenused')::JSONB
+                                  ) AS x(nomid INTEGER, hind NUMERIC, kogus NUMERIC, kood TEXT, nimetus TEXT)
+                                  INNER JOIN libs.nomenklatuur n ON n.id = x.nomid
+                     ) qry
+               )::TEXT                                          AS gridData
+        FROM libs.library l
+        WHERE l.library = 'LAPSE_GRUPP'
+          AND l_nom_id IN (
+            SELECT qry.nomid
+            FROM (
+                     SELECT nomid, hind
+                     FROM jsonb_to_recordset(properties::JSONB -> 'teenused') AS x(nomid INTEGER, hind NUMERIC)) qry
+                     INNER JOIN libs.nomenklatuur n ON n.id = qry.nomid
+            WHERE qry.hind <> n.hind
+        )
+        LOOP
+            -- salvestame grupp
+            SELECT row_to_json(row) INTO json_object
+            FROM (SELECT v_groups.id                   AS id,
+                         (SELECT to_jsonb(v_groups.*)) AS data) row;
+
+            SELECT lapsed.sp_salvesta_lapse_grupp(json_object :: JSONB, user_id, l_rekvid) INTO l_grupp_id;
+
+            IF (l_grupp_id IS NOT NULL AND l_grupp_id > 0)
+            THEN
+                l_count = l_count + 1;
+            END IF;
+
+        END LOOP;
+
 
     -- делаем выборку услуг
 
@@ -35,19 +84,19 @@ BEGIN
                n.hind,
                lk.parentid,
                lk.tunnus,
-               lk.properties ->> 'yksus' as yksus,
-               lk.properties ->> 'all_yksus' as all_yksus,
-               lk.properties ->> 'soodus' as soodus,
-               lk.properties ->> 'kas_protsent' as kas_protsent ,
-               lk.properties ->> 'kas_eraldi' as kas_eraldi,
-               lk.properties ->> 'kas_ettemaks' as kas_ettemaks,
-               lk.properties ->> 'ettemaksu_period' as ettemaksu_period,
-               lk.properties ->> 'kas_inf3' as kas_inf3,
-               lk.properties ->> 'sooduse_alg' as sooduse_alg,
-               lk.properties ->> 'sooduse_lopp' as sooduse_lopp,
-               lk.properties ->> 'alg_kpv' as alg_kpv,
-               lk.properties ->> 'lopp_kpv' as lopp_kpv,
-               lk.properties ->> 'kogus' as kogus,
+               lk.properties ->> 'yksus'            AS yksus,
+               lk.properties ->> 'all_yksus'        AS all_yksus,
+               lk.properties ->> 'soodus'           AS soodus,
+               lk.properties ->> 'kas_protsent'     AS kas_protsent,
+               lk.properties ->> 'kas_eraldi'       AS kas_eraldi,
+               lk.properties ->> 'kas_ettemaks'     AS kas_ettemaks,
+               lk.properties ->> 'ettemaksu_period' AS ettemaksu_period,
+               lk.properties ->> 'kas_inf3'         AS kas_inf3,
+               lk.properties ->> 'sooduse_alg'      AS sooduse_alg,
+               lk.properties ->> 'sooduse_lopp'     AS sooduse_lopp,
+               lk.properties ->> 'alg_kpv'          AS alg_kpv,
+               lk.properties ->> 'lopp_kpv'         AS lopp_kpv,
+               lk.properties ->> 'kogus'            AS kogus,
                lk.muud
         FROM lapsed.lapse_kaart lk
                  INNER JOIN libs.nomenklatuur n ON n.id = lk.nomid
@@ -97,5 +146,5 @@ GRANT EXECUTE ON FUNCTION lapsed.update_teenuste_hinnad(INTEGER, INTEGER, DATE) 
 
 /*
 select * from lapsed.lapse_kaart  order by id desc limit 10
-select lapsed.update_teenuste_hinnad(70, 2738)
+select lapsed.update_teenuste_hinnad(70, 738)
  */
