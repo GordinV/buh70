1445e7a73e8e68b085f9796ff5755d540944e338
Vladislav Gordin <vladislav.gordin@gmail.com>
2019-11-21 12:02:05 +0200

    Task 4702 arv numeration

---
+++ sql/lapsed/create_number_sequence.sql
@@ -0,0 +1,59 @@
+DROP FUNCTION IF EXISTS docs.create_number_sequence(INTEGER, TEXT);
+
+CREATE FUNCTION docs.create_number_sequence(l_rekvid INTEGER, l_dok TEXT)
+    RETURNS TEXT
+    LANGUAGE plpgsql
+AS
+$$
+DECLARE
+    l_sequence_name TEXT = lower(l_dok) || '_' || l_rekvid::TEXT || '_number';
+    l_sql           TEXT;
+    l_number        TEXT;
+    l_result INTEGER;
+BEGIN
+    -- sequence name
+    -- check if exists sequencetbl
+    IF NOT EXISTS(
+            SELECT 1 FROM pg_class WHERE relname = l_sequence_name)
+    THEN
+        -- IF NOT then sql for create sequence
+
+        -- get last doc number
+        l_sql = 'select (max(SUBSTRING(''0'' || coalesce(tbl.number,''0''), ' || quote_literal('Y*[0-9]\d+') ||
+                ')::bigint) ::bigint) from docs.' || l_dok || ' tbl where rekvid = $1 ';
+        EXECUTE l_sql INTO l_number USING l_rekvid;
+
+        l_sql = 'CREATE SEQUENCE ' || l_sequence_name || ' AS integer;' ||
+                'GRANT ALL ON SEQUENCE ' || l_sequence_name || ' TO public;';
+
+        if l_number is not null and l_number::INTEGER > 0 THEN
+            -- will store last value
+            l_sql = l_sql || 'select setval(' || quote_literal(l_sequence_name) || ',' || l_number || ');';
+
+        END IF;
+        -- execute sequnce
+        EXECUTE l_sql into l_result;
+    END IF;
+
+    -- return name of sequence
+
+    RETURN l_sequence_name;
+END;
+$$;
+
+GRANT EXECUTE ON FUNCTION docs.create_number_sequence(INTEGER, TEXT) TO arvestaja;
+GRANT EXECUTE ON FUNCTION docs.create_number_sequence(INTEGER, TEXT) TO dbkasutaja;
+GRANT EXECUTE ON FUNCTION docs.create_number_sequence(INTEGER, TEXT) TO dbpeakasutaja;
+
+/*
+select docs.create_number_sequence(63, 'ARV')
+DROP SEQUENCE ARV_63_number
+ALTER SEQUENCE arv_63_number START  WITH 43
+SELECT * FROM pg_class WHERE relname = 'arv_63_number'
+
+select setval('arv_63_number', 43)
+select currval('arv_' || 63::text || '_number')
+
+SELECT * FROM DOCS.ARV
+select (max(SUBSTRING('0' || coalesce(tbl.number,'0'), E'Y*[0-9]\\d+')::bigint) ::bigint) from docs.ARV tbl where rekvid = 63
+ */
--- sql/raama/docs.sp_get_number.sql
+++ sql/raama/docs.sp_get_number.sql
@@ -1,92 +1,105 @@
-﻿DROP FUNCTION IF EXISTS sp_get_number(tnrekvid INTEGER, tcdok TEXT, tnyear INTEGER, tndokpropid INTEGER );
-DROP FUNCTION IF EXISTS docs.sp_get_number(tnrekvid INTEGER, tcdok TEXT, tnyear INTEGER, tndokpropid INTEGER );
+﻿DROP FUNCTION IF EXISTS sp_get_number(tnrekvid INTEGER, tcdok TEXT, tnyear INTEGER, tndokpropid INTEGER);
+DROP FUNCTION IF EXISTS docs.sp_get_number(tnrekvid INTEGER, tcdok TEXT, tnyear INTEGER, tndokpropid INTEGER);
 
 CREATE FUNCTION docs.sp_get_number(tnrekvid INTEGER, tcdok TEXT, tnyear INTEGER, tndokpropid INTEGER)
-  RETURNS TEXT
-LANGUAGE plpgsql
-AS $$
+    RETURNS TEXT
+    LANGUAGE plpgsql
+AS
+$$
 DECLARE
-  v_number          RECORD;
-  lcPref            TEXT = '%';
-  lcNumber          TEXT = '0';
-  lcTableName       TEXT;
-  lcAdditionalWhere TEXT = '';
-  lcSqlString       TEXT;
+    v_number          RECORD;
+    lcPref            TEXT = '%';
+    lcNumber          TEXT = '0';
+    lcTableName       TEXT;
+    lcAdditionalWhere TEXT = '';
+    lcSqlString       TEXT;
+    l_seq_name        TEXT;
 BEGIN
 
-  IF tnDokPropId IS NOT NULL
-  THEN
-    SELECT ltrim(rtrim(proc_))
-    INTO lcPref
-    FROM libs.dokprop
-    WHERE id = tnDokPropId;
-  END IF;
+    IF tnDokPropId IS NOT NULL
+    THEN
+        SELECT ltrim(rtrim(proc_)) INTO lcPref
+        FROM libs.dokprop
+        WHERE id = tnDokPropId;
+    END IF;
 
-  lcPref = coalesce(lcPref, '%');
+    lcPref = coalesce(lcPref, '%');
 
-  CASE
-    WHEN tcDok = 'ARV'
-    THEN
-      lcTableName = 'docs.arv';
-      lcAdditionalWhere = ' and liik = 0 ';
-    WHEN tcDok = 'SARV'
-      THEN
-        lcTableName = 'docs.arv';
-        lcAdditionalWhere = ' and liik = 1 and operid is not null and not empty(operid)';
-    WHEN tcDok = 'SORDER'
-    THEN
-      lcTableName = 'docs.korder1';
-      lcAdditionalWhere = ' and tyyp = 1 ';
-    WHEN tcDok = 'VORDER'
-    THEN
-      lcTableName = 'docs.korder1';
-      lcAdditionalWhere = ' and tyyp = 2 ';
-    WHEN tcDok = 'MK'
-    THEN
-      lcTableName = 'docs.mk';
-      lcAdditionalWhere = ' and OPT = 1 ';
-    WHEN tcDok = 'SMK'
-    THEN
-      lcTableName = 'docs.mk';
-      lcAdditionalWhere = ' and OPT = 1 ';
-    WHEN tcDok = 'VMK'
-    THEN
-      lcTableName = 'docs.mk';
-      lcAdditionalWhere = ' and OPT = 2 ';
-    WHEN tcDok = 'LEPING'
-    THEN
-      lcTableName = 'docs.leping1';
-    WHEN tcDok = 'TAOTLUS'
-    THEN
-      lcTableName = 'eelarve.taotlus';
-    WHEN tcDok = 'LUBA'
-    THEN
-      lcTableName = '(select left(l.number,2)::text as number, l.parentid, l.rekvid, l.algkpv as kpv from rekl.luba l)';
-  END CASE;
+    CASE
+        WHEN tcDok = 'ARV'
+            THEN
+                lcTableName = 'docs.arv';
+                lcAdditionalWhere = ' and liik = 0 ';
 
-  -- building sql query with regexp for only numbers
-  lcSqlString = 'select (max(SUBSTRING(''0'' || coalesce(tbl.number,''0''), ' || quote_literal('Y*[0-9]\d+') ||
-                ')::bigint) ::bigint) as number from docs.doc d inner join '
-                || lcTableName || ' tbl on d.id = tbl.parentid and d.status <> 3 '
-                || ' where tbl.rekvId = $1::integer and year(tbl.kpv) = $2::integer and encode(tbl.number::bytea, ''escape'')::text  ilike $3::text';
+                l_seq_name = docs.create_number_sequence(tnrekvid, tcDok);
+                SELECT nextval(l_seq_name) AS number INTO v_number;
+        WHEN tcDok = 'SARV'
+            THEN
+                lcTableName = 'docs.arv';
+                lcAdditionalWhere = ' and liik = 1 and operid is not null and not empty(operid)';
+        WHEN tcDok = 'SORDER'
+            THEN
+                lcTableName = 'docs.korder1';
+                lcAdditionalWhere = ' and tyyp = 1 ';
+        WHEN tcDok = 'VORDER'
+            THEN
+                lcTableName = 'docs.korder1';
+                lcAdditionalWhere = ' and tyyp = 2 ';
+        WHEN tcDok = 'MK'
+            THEN
+                lcTableName = 'docs.mk';
+                lcAdditionalWhere = ' and OPT = 1 ';
+        WHEN tcDok = 'SMK'
+            THEN
+                lcTableName = 'docs.mk';
+                lcAdditionalWhere = ' and OPT = 1 ';
+        WHEN tcDok = 'VMK'
+            THEN
+                lcTableName = 'docs.mk';
+                lcAdditionalWhere = ' and OPT = 2 ';
+        WHEN tcDok = 'LEPING'
+            THEN
+                lcTableName = 'docs.leping1';
+        WHEN tcDok = 'TAOTLUS'
+            THEN
+                lcTableName = 'eelarve.taotlus';
+        WHEN tcDok = 'LUBA'
+            THEN
+                lcTableName =
+                        '(select left(l.number,2)::text as number, l.parentid, l.rekvid, l.algkpv as kpv from rekl.luba l)';
+        END CASE;
 
-  lcSqlString = lcSqlString || lcAdditionalWhere;
-  EXECUTE lcSqlString
-  INTO v_number
-  USING tnRekvId, tnYear, lcPref;
+    IF v_number IS NULL
+    THEN
+        -- building sql query with regexp for only numbers
+        lcSqlString = 'select (max(SUBSTRING(''0'' || coalesce(tbl.number,''0''), ' || quote_literal('Y*[0-9]\d+') ||
+                      ')::bigint) ::bigint) as number from docs.doc d inner join '
+            || lcTableName || ' tbl on d.id = tbl.parentid and d.status <> 3 '
+            ||
+                      ' where tbl.rekvId = $1::integer and year(tbl.kpv) = $2::integer and encode(tbl.number::bytea, ''escape'')::text  ilike $3::text';
 
-  -- will plus pref and encrement
+        lcSqlString = lcSqlString || lcAdditionalWhere;
+        EXECUTE lcSqlString
+            INTO v_number
+            USING tnRekvId, tnYear, lcPref;
+    END IF;
 
-  IF lcPref = '%'
-  THEN
-    lcPref = '';
-  END IF;
+    -- will plus pref and encrement
 
-  lcNumber = lcPref || (coalesce(v_number.number, 0) + 1) :: TEXT;
+    IF lcPref = '%'
+    THEN
+        lcPref = '';
+    END IF;
 
-  RETURN lcNumber;
+    lcNumber = lcPref || (coalesce(v_number.number, 0) + 1) :: TEXT;
+
+    RETURN lcNumber;
 END;
 $$;
+
+GRANT EXECUTE ON FUNCTION docs.sp_get_number(tnrekvid INTEGER, tcdok TEXT, tnyear INTEGER, tndokpropid INTEGER) TO arvestaja;
+GRANT EXECUTE ON FUNCTION docs.sp_get_number(tnrekvid INTEGER, tcdok TEXT, tnyear INTEGER, tndokpropid INTEGER) TO dbkasutaja;
+GRANT EXECUTE ON FUNCTION docs.sp_get_number(tnrekvid INTEGER, tcdok TEXT, tnyear INTEGER, tndokpropid INTEGER) TO dbpeakasutaja;
 
 
 /*
