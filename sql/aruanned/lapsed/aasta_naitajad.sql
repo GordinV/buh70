636b724118e50f757c4cb8354af37d0795d2fd27
Vladislav Gordin <vladislav.gordin@gmail.com>
2020-02-25 17:20:41 +0200

    task 5246 remove liik from report

--- sql/aruanned/lapsed/aasta_naitajad.sql
+++ sql/aruanned/lapsed/aasta_naitajad.sql
@@ -26,23 +26,23 @@ CREATE OR REPLACE FUNCTION lapsed.aasta_naitajad(l_rekvid INTEGER,
     ) AS
 $BODY$
 
-SELECT l_kpv                                              AS period,
+SELECT l_kpv                                                   AS period,
        rekvid,
        liik::TEXT,
-       array_to_string(array_agg(DISTINCT yksused),',')::TEXT AS yksused,
-       count(*)::INTEGER                                  AS lapsed_kokku,
-       count(laps_id) FILTER ( WHERE kuu = 1 )::INTEGER   AS jaanuar,
-       count(laps_id) FILTER ( WHERE kuu = 2 )::INTEGER   AS veebruar,
-       count(laps_id) FILTER ( WHERE kuu = 3 )::INTEGER   AS marts,
-       count(laps_id) FILTER ( WHERE kuu = 4 )::INTEGER   AS apriil,
-       count(laps_id) FILTER ( WHERE kuu = 5 )::INTEGER   AS mai,
-       count(laps_id) FILTER ( WHERE kuu = 6 )::INTEGER   AS juuni,
-       count(laps_id) FILTER ( WHERE kuu = 7 )::INTEGER   AS juuli,
-       count(laps_id) FILTER ( WHERE kuu = 8 )::INTEGER   AS august,
-       count(laps_id) FILTER ( WHERE kuu = 9 )::INTEGER   AS september,
-       count(laps_id) FILTER ( WHERE kuu = 10 )::INTEGER  AS oktoober,
-       count(laps_id) FILTER ( WHERE kuu = 11 )::INTEGER  AS november,
-       count(laps_id) FILTER ( WHERE kuu = 12 )::INTEGER  AS detsember
+       array_to_string(array_agg(DISTINCT yksused), ',')::TEXT AS yksused,
+       count(*)::INTEGER                                       AS lapsed_kokku,
+       count(laps_id) FILTER ( WHERE kuu = 1 )::INTEGER        AS jaanuar,
+       count(laps_id) FILTER ( WHERE kuu = 2 )::INTEGER        AS veebruar,
+       count(laps_id) FILTER ( WHERE kuu = 3 )::INTEGER        AS marts,
+       count(laps_id) FILTER ( WHERE kuu = 4 )::INTEGER        AS apriil,
+       count(laps_id) FILTER ( WHERE kuu = 5 )::INTEGER        AS mai,
+       count(laps_id) FILTER ( WHERE kuu = 6 )::INTEGER        AS juuni,
+       count(laps_id) FILTER ( WHERE kuu = 7 )::INTEGER        AS juuli,
+       count(laps_id) FILTER ( WHERE kuu = 8 )::INTEGER        AS august,
+       count(laps_id) FILTER ( WHERE kuu = 9 )::INTEGER        AS september,
+       count(laps_id) FILTER ( WHERE kuu = 10 )::INTEGER       AS oktoober,
+       count(laps_id) FILTER ( WHERE kuu = 11 )::INTEGER       AS november,
+       count(laps_id) FILTER ( WHERE kuu = 12 )::INTEGER       AS detsember
 FROM (
          WITH qryPeriod AS (
              SELECT CASE
@@ -52,7 +52,7 @@ FROM (
               qry_liik AS (
                   SELECT DISTINCT coalesce((n.properties ->> 'oppe_tyyp')::TEXT, 'Põhiõpe')::TEXT       AS liik,
                                   d.id,
-                                  array_agg(DISTINCT lg.kood::TEXT || '-' || lg.liik || '-' || lg.tyyp) AS yksused
+                                  array_agg(DISTINCT lg.kood::TEXT || '-' || coalesce(r.properties ->>'liik','') || '-' || lg.tyyp) AS yksused
                   FROM qryPeriod,
                        docs.doc d
                            INNER JOIN docs.arv a ON d.id = a.parentid
@@ -62,16 +62,6 @@ FROM (
                            INNER JOIN lapsed.lapse_kaart lk ON lk.parentid = l.parentid AND lk.nomid = a1.nomid
                            INNER JOIN (SELECT rekvid,
                                               kood,
-                                              CASE
-                                                  WHEN coalesce((l.properties::JSONB ->> 'liik')::INTEGER, 1) = 1
-                                                      THEN 'LASTEAED'
-                                                  WHEN COALESCE((l.properties::JSONB ->>
-                                                                 'liik')::INTEGER, 1) = 2 THEN
-                                                      'HUVIKOOL'
-                                                  WHEN COALESCE((l.properties::JSONB ->>
-                                                                 'liik')::INTEGER, 1) = 3 THEN
-                                                      'KOOL'
-                                                  END AS liik,
                                               CASE
                                                   WHEN COALESCE((l.properties::JSONB ->>
                                                                  'tyyp')::INTEGER, 1) = 1 THEN 'LASTEAJARÜHM'
@@ -85,11 +75,13 @@ FROM (
                                        FROM libs.library l
                                        WHERE library = 'LAPSE_GRUPP'
                        ) lg ON lg.rekvid = d.rekvid AND lg.kood::TEXT = lk.properties ->> 'yksus'
+                           INNER JOIN ou.rekv r ON r.id = a.rekvid
                   WHERE year(a.kpv) = year(qryPeriod.kpv)
                     AND a.kpv <= qryPeriod.kpv
-                    AND (l_liik IS NULL OR lg.liik ILIKE l_liik || '%')
+                    AND (l_liik IS NULL OR coalesce(r.properties ->>'liik','') ILIKE l_liik || '%')
                     AND (l_tyyp IS NULL OR lg.tyyp ILIKE l_tyyp || '%')
-                  GROUP BY (n.properties ->> 'oppe_tyyp'), d.id
+                      GROUP BY (n.properties ->> 'oppe_tyyp')
+                      , d.id
               )
          SELECT DISTINCT l.parentid                             AS laps_id,
                          d.rekvid,
@@ -106,7 +98,8 @@ FROM (
      ) qry
 WHERE qry.rekvid IN (SELECT rekv_id
                      FROM get_asutuse_struktuur(l_rekvid))
-GROUP BY liik, rekvid
+    GROUP BY liik
+    , rekvid
 
 $BODY$
     LANGUAGE SQL
@@ -122,6 +115,6 @@ GRANT EXECUTE ON FUNCTION lapsed.aasta_naitajad(INTEGER, DATE, TEXT, TEXT) TO db
 
 /*
 SELECT *
-FROM lapsed.aasta_naitajad(63, '2019-11-30')
+FROM lapsed.aasta_naitajad(63, '2020-12-31')
 
 */
