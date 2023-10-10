3985638ee321059226a6d5fdc00698a9711356d0
Vladislav Gordin <vladislav.gordin@gmail.com>
2023-10-10 15:19:06 +0300

    update: arve kaibed info

--- models/lapsed/arv.js
+++ models/lapsed/arv.js
@@ -46,13 +46,13 @@ const Arv = {
                                        'kr', sum(kb.kr),
                                        'lopp_db', sum(kb.lopp_db),
                                        'lopp_kr', sum(kb.lopp_kr)) AS kaibed,
-                    kb.isik_id
+                    kb.isik_id, kb.rekvid
              FROM arved a,
                   lapsed,
                   lapsed.saldo_ja_kaibeandmik(a.rekvid,
                                               make_date(year(a.alg_kpv), month(a.alg_kpv), 01)::DATE,
                                               gomonth(make_date(year(a.lopp_kpv), month(a.lopp_kpv), 01), 1) - 1) kb
-             GROUP BY kb.isik_id
+             GROUP BY kb.isik_id, kb.rekvid
          ),
          details AS (
              SELECT a.parentid,
@@ -153,7 +153,7 @@ const Arv = {
                          a.properties ->> 'ettemaksu_period'                       AS ettemaksu_period,
                          va.properties ->> 'pank'                                  AS pank,
                          REPLACE((va.properties ->> 'iban')::TEXT, E'\r', '')  AS iban,
-                       to_jsonb(array((SELECT kaibed FROM kaibed WHERE kaibed.isik_id = l.id))) AS kaibed,
+                       to_jsonb(array((SELECT kaibed FROM kaibed WHERE kaibed.isik_id = l.id and kaibed.rekvid = d.rekvid))) AS kaibed,
                        to_jsonb(array((SELECT details FROM details det WHERE det.parentid = d.id)))   AS details,
                         r.muud as tais_nimetus,
                         r.tel as rekv_tel,
