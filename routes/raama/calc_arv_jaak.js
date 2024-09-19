9d02a8be22fd2fb6fab0da00d1713bb0f39b0e0b
GordinV <vladislav.gordin@gmail.com>
2024-09-19 09:04:04 +0300

    new: calc arv jaak script added to cron

--- routes/raama/calc_arv_jaak.js
+++ routes/raama/calc_arv_jaak.js
@@ -6,12 +6,14 @@ const config = require('./../../config/narvalv.json');
 let sql = `SELECT docs.check_arv_jaak(a.parentid, u.id)
            from docs.arv a,
                 ou.userid u
-           where coalesce(a.tasud, current_date + 1) < current_date
-             and a.jaak <> 0
+           where tasud is not null
+             and a.tasud <= current_date
+             and a.jaak > 0
              and a.liik = 1
              and u.rekvid = a.rekvid
-             and u.kasutaja = ''vlad''
-           order by a.kpv
+             and coalesce(a.tahtaeg, current_date) <= current_date
+             and u.kasutaja = 'vlad'
+           order by a.tasud desc, a.kpv
            limit 5000;`;
 
 let data = db.queryDb(sql, null, null, null, null, null, config);
