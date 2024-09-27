66e286014c6df417d57535d8e75d21bec53f6d1c
GordinV <vladislav.gordin@gmail.com>
2024-09-27 14:22:03 +0300

    new: lisa1_lisa5_kontrol updated parameters

--- routes/eelarve/lisa1_lisa5_kontrol.js
+++ routes/eelarve/lisa1_lisa5_kontrol.js
@@ -3,10 +3,8 @@ const db = require('./../../libs/db');
 const config = require('./../../config/narvalv.json');
 
 // получить отчет
-// l_kpv DATE,
-//                                                        l_rekvid INTEGER,
-//                                                        l_kond
-let sql = `SELECT eelarve.koosta_lisa_1_5_kontrol(qry.kpv::date, qry.rekvid, qry.kond) as tulemus
+
+let sql = `SELECT eelarve.koosta_lisa_1_5_kontrol(2477, qry.kpv::date) as tulemus
            FROM (
                     SELECT DISTINCT  (make_date(year(kpv), month(kpv), 1) + interval '1 month')::date - 1 as kpv,
                                      1 AS tyyp, 1 AS kond, 63 as rekvid
