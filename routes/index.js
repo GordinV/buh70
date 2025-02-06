fc90d5eda5a6ebe01bf5b7224d32b8c42c1a29a5
GordinV <vladislav.gordin@gmail.com>
2025-02-06 14:35:25 +0200

    new:updated smk register, added new task

--- routes/index.js
+++ routes/index.js
@@ -133,7 +133,7 @@ module.exports = function (app) {
     app.post('/calc/loe_makse', checkAuth, require('./lapsed/loe_makse').post); //checkAuth
     app.post('/calc/ebatoenaolised', checkAuth, require('./raama/ebatoenaolised').post); //checkAuth
     app.post('/calc/importAsendusTaabel', checkAuth, require('./lapsed/importAsendusTaabel').post); //checkAuth
-//    app.post('/calc/TuhistaMakseJaotamine', checkAuth, require('./lapsed/TuhistaMakseJaotamine').post); //checkAuth
+    app.post('/calc/TuhistaMakseJaotamine', checkAuth, require('./lapsed/tuhistaMakseJaotamine').post); //checkAuth
 
     //
     app.post('/calc/:taskName', checkAuth, require('./calc').post); //checkAuth
