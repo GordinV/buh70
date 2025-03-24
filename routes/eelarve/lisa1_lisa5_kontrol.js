c6b29acb33aa050c90060fd8e8fff3d4dea616e3
GordinV <vladislav.gordin@gmail.com>
2025-03-24 22:29:04 +0200

    new:updated lisa 1 - 5 kontrol

--- routes/eelarve/lisa1_lisa5_kontrol.js
+++ routes/eelarve/lisa1_lisa5_kontrol.js
@@ -2,15 +2,65 @@
 const db = require('./../../libs/db');
 const config = require('./../../config/narvalv.json');
 
-// получить отчет
 
-let sql = `SELECT eelarve.koosta_lisa_1_5_kontrol(2477, qry.kpv::date) as tulemus
-           FROM (
-                    SELECT DISTINCT  (make_date(year(kpv), month(kpv), 1) + interval '1 month')::date - 1 as kpv,
-                                     1 AS tyyp, 1 AS kond, 63 as rekvid
-                    FROM docs.doc d
-                             INNER JOIN docs.journal j ON j.parentid = d.id
-                    WHERE d.lastupdate >= current_date - INTERVAL '1 day'
-                ) qry`;
+async function get_rekv_data() {
+// получить параметры
+    let sql = `select distinct
+                   gomonth(make_date(aasta, kuu, 1), 1) - 1 as kpv,
+                   rekvid
+               from
+                   ou.aasta
+               where
+                     kinni = 0
+                 and gomonth(make_date(aasta, kuu, 1), 1) < gomonth(current_date, 1)
+                 and rekvid in (
+                                   SELECT
+                                       rekv_id
+                                   FROM
+                                       get_asutuse_struktuur(63) a
 
-let data = db.queryDb(sql, null, null, null, null, null, config);
+                               )
+                 and make_date(aasta, kuu, 1) > '2025-01-01'
+               order by
+                   rekvid, kpv`;
+
+    let data = await db.queryDb(sql, null, null, null, null, null, config);
+    return data;
+
+}
+
+let promise = new Promise((resolve, reject) => {
+
+// получить список работников
+    let sql = `select distinct
+                   to_char((gomonth(make_date(aasta, kuu, 1), 1) - 1)::date, 'YYYY-MM-DD') as kpv,
+                   rekvid
+               from
+                   ou.aasta
+               where
+                     kinni = 0
+                 and gomonth(make_date(aasta, kuu, 1), 1) < gomonth(current_date, 1)
+                 and rekvid in (
+                                   SELECT
+                                       rekv_id
+                                   FROM
+                                       get_asutuse_struktuur(63) a
+
+                               )
+                 and make_date(aasta, kuu, 1) > '2025-01-01'
+               order by
+                   rekvid, kpv`;
+
+    let data = db.queryDb(sql, null, null, null, null, null, config);
+    resolve(data);
+}).then((data) => {
+    console.log(data);
+    var sqls = [];
+    for (let i = 0; i < data.data.length; i++) {
+        let row = data.data[i];
+        let sql = `SELECT eelarve.salvesta_lisa_1_5_kontrol(2477, '${row.kpv}',${row.rekvid})`;
+        sqls.push(sql);
+    }
+    let result = db.executeQueries(sqls, null, null, config);
+})
+
