6a74b24eabeee4ea2a59ba07dd8742256da4b8a3
GordinV <vladislav.gordin@gmail.com>
2026-07-10 14:41:23 +0300

    update: new AI model

--- routes/ai/check_tsd.js
+++ routes/ai/check_tsd.js
@@ -19,11 +19,14 @@ async function parseAddress(addressStr) {
     const genAI = new GoogleGenerativeAI(API_KET);
     const model = genAI.getGenerativeModel({
         model: "gemini-3.5-flash",
+//        model: "gemini-1.5-flash",
         generationConfig: {
             responseMimeType: "application/json",
             temperature: 0.1
         },
     });
+
+    console.log('addressStr', addressStr)
 
     const prompt = `
     Role: You are a high-precision Address Parser specializing in Estonian geography.
