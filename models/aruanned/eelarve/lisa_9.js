645684c2ab56606335ed49ebcbe176c3f8f0faf8
GordinV <vladislav.gordin@gmail.com>
2026-09-11 09:41:10 +0300

    update: lisa_9 parandused

--- models/aruanned/eelarve/lisa_9.js
+++ models/aruanned/eelarve/lisa_9.js
@@ -10,12 +10,31 @@ module.exports = {
             {id: "lopp_db", name: "Lõpp deebet", width: "100px"},
             {id: "lopp_kr", name: "Lõpp kreedit", width: "200px"}
         ],
-        sqlString: `SELECT qryReport.*, r.nimetus as asutus
-                    FROM eelarve.lisa_9($1::DATE, $2::DATE, $3::INTEGER, $4::INTEGER) qryReport
-                             INNER JOIN ou.rekv r ON r.id = qryReport.rekv_id
-                    ORDER BY maksja_regkood, asutus, saaja_nimi, kpv, artikkel, tegev
+        sqlString: `with
+                        report as (
+                                      SELECT
+                                          qryReport.*,
+                                          sum(qryReport.summa) over (partition by saaja_regkood, maksja_regkood, tehingu_arv) as saaja_summa,
+                                          r.nimetus                                              as asutus
+                                      FROM
+                                          eelarve.lisa_9($1::DATE, $2::DATE, $3::INTEGER, $4::INTEGER) qryReport
+                                              INNER JOIN ou.rekv                                       r ON r.id = qryReport.rekv_id
+                        )
+                    select *
+                    from
+                        report r
+                    where
+                        r.saaja_summa > 0
+                    ORDER BY
+                        maksja_regkood, asutus, saaja_nimi, kpv, artikkel, tegev
         `,     // $1 - alg_kpv $2 - lopp_kpv, $3 - rekvid, $4 - kond
         params: '',
         alias: 'lisa_9'
-    }
+    },
+    ai : {
+        endpoint: `https://check-xml-1088344506167.europe-north1.run.app`,
+        type: `link`,
+        alias: `ai`
+    },
+
 };
